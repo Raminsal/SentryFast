9c50b9d71b0120290e4a352364c9b84404a60269
TeleSentry <raminsalofficial@gmail.com>
2018-06-19 16:27:58 +0430

    Add files via upload

---
+++ tdbot/Config.lua
@@ -0,0 +1,25 @@
+do
+local _ = {
+		SUDO = 159887854,
+		sudoidforpanel = 159887854,
+		cliidforpanel = 294299210,
+        SUDO_ID = {159887854,325744113,294299210},
+		sudoapibot = {159887854,294299210},
+        Full_Sudo = {159887854,325744113,294299210},
+        ChannelLogs= -100000000000,
+		token = '325744113:AAHXpPinUHQqkvzj6TbxQKX3Crg86uE9QLs',
+        TokenApibot = '180516739:AAENSVLbo0d0qGmLoszBvHZOwPp1bXv-WmQ',
+        ChannelUsername = '@FreeManagerChannelBOT',
+        Channelforcejoin = '@FreeManagerChannelBOT',
+	    Sudousernameapi = 'sale_abot',
+        Channelusernameapi = 'FreeManagerChannelBOT',
+        Botusernameapi = 'FreeManager_BOT',
+		salechannelbot = 'sale_group',
+        Pardakht = 'https://zarinp.al/95788',
+        Botusernamelink = 'FreeManager_BOT',
+        Sendpayiduser = 159887854,
+        apipanelbotuserid = 325744113,
+        Channelnameauto = 'ربات مدیریت گروه',
+}
+return _
+end
